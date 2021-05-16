1470ddce817f70c962213e96a18f07b895590615
Brandon Miller <brandonkmiller@protonmail.com>
2021-05-16 00:30:33 -0400

    Added a .pylintrc and script for linting command
    Preparing to add workflows support

--- __init__.py
+++ __init__.py
@@ -1,7 +1,8 @@
+# pylint: disable=missing-docstring
+
 from binaryninja import PluginCommand
 from .helper import run_uefi_helper
 from .teloader import TerseExecutableView
 
 PluginCommand.register('UEFI Helper', 'Run UEFI Helper analysis', run_uefi_helper)
 TerseExecutableView.register()
-
---
+++ lint.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+pylint3 --reports=y --rcfile .pylintrc .
