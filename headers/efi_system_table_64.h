9d8b61d1dc78e7b17464ca9efe34d048c4100e34
Jordan Wiens <jordan@psifertex.com>
2023-04-25 19:54:14 -0400

    support for 3.1 clang type parsing and a few other upcoming API deprecations

--- headers/efi_system_table_64.h
+++ headers/efi_system_table_64.h
@@ -3,6 +3,22 @@
 #define __int32 int
 #define __int64 long long
 
+typedef uint8_t BOOLEAN;
+typedef uint8_t UINT8;
+typedef uint16_t UINT16;
+typedef uint32_t UINT32;
+typedef uint64_t UINT64;
+typedef int8_t INT8;
+typedef int16_t INT16;
+typedef int32_t INT32;
+typedef int64_t INT64;
+typedef struct {
+  UINT32  Data1;
+  UINT16  Data2;
+  UINT16  Data3;
+  UINT8   Data4[8];
+} GUID;
+
 struct EFI_DEVICE_PATH_PROTOCOL;
 struct EFI_TIME;
 struct EFI_BOOT_SERVICES;
